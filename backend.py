fac9a950d4c8ffb5710b35951e1023e94e77f1ab
Valentino Artizzu <tizzu93@gmail.com>
2024-11-13 16:52:49 +0100

    Add profile management features: create, delete, and rename profiles; update UI for profile selection
    It needs to be fixed in terms of profile button coloring, otherwise it's working

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ import win32gui
 import win32process
 import psutil
 import time
-import sys
+import sys, os
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtUiTools import QUiLoader
 
@@ -60,6 +60,7 @@ class DropLabel(QtWidgets.QPushButton):
             self.setText("None")
             save_profile()
 
+
 def save_profile():
     profiles.save_profile(current_profile, f'profiles/{current_profile.profile_name}.json')   
 
@@ -79,6 +80,29 @@ dialog = loader.load("resources/interface.ui", None)
 dialog.setWindowTitle("BagOnKey")
 dialog.setWindowIcon(QtGui.QIcon("assets/icon.png"))
 dialog.show()
+
+profiles_list = dialog.profileList
+profiles_list_widget = QtWidgets.QWidget()
+profiles_list_content = QtWidgets.QVBoxLayout()
+
+profiles_list_widget.setLayout(profiles_list_content)
+profiles_list.setWidget(profiles_list_widget)
+
+
+# for each profile in the profiles folder, add it to the list
+
+def reload_buttons():
+    # clean the layout
+    for i in reversed(range(profiles_list_content.count())):
+        profiles_list_content.itemAt(i).widget().deleteLater()
+    for profile in os.listdir("profiles"):
+        profile_button = QtWidgets.QPushButton(profile.split(".")[0])
+        profile_button.setStyleSheet("border: 1px solid black; padding: 5px;")
+        def on_profile_click(profile_name):
+            global current_profile
+            current_profile = load_profile(profile_name)
+        profile_button.clicked.connect(lambda _, pn = profile.split(".")[0]: on_profile_click(pn))
+        profiles_list_content.addWidget(profile_button)
 
 key_to_button = {
     'f13': dialog.button1,
@@ -119,6 +143,12 @@ def load_profile(profile_name):
             button.setText("None")
         else: 
             button.setText(profile.layout[i].name)
+    # get the profiles list and make a green border to the selected profile, and a black border to the others
+    for i in range(profiles_list_content.count()):
+        if profiles_list_content.itemAt(i).widget().text() == profile_name:
+            profiles_list_content.itemAt(i).widget().setStyleSheet("border: 3px solid green; padding: 5px;")
+        else:
+            profiles_list_content.itemAt(i).widget().setStyleSheet("border: 1px solid black; padding: 5px;")
     return profile
 
 current_profile = load_profile("default")
@@ -203,7 +233,6 @@ def refresh_description(desc):
         description_layout.itemAt(i).widget().deleteLater()
     description_layout.addWidget(QtWidgets.QLabel(desc))
 
-
 # Start the background thread
 threading.Thread(target=monitor_foreground_process_thread, daemon=True).start()
 
@@ -226,6 +255,113 @@ class DragButton(QtWidgets.QPushButton):
 
             drag.exec(QtCore.Qt.DropAction.MoveAction)
             self.show() 
+
+def delete_dialog(profile_name):
+    delete_dialog = QtWidgets.QDialog()
+    delete_dialog.setWindowTitle("Delete Profile")
+    delete_dialog.setWindowIcon(QtGui.QIcon("assets/icon.png"))
+    delete_dialog.resize(200, 100)
+    layout = QtWidgets.QVBoxLayout()
+    delete_dialog.setLayout(layout)
+    # if name is default say it's not possible to delete
+    if profile_name == "default":
+        label = QtWidgets.QLabel("You cannot delete the default profile")
+        layout.addWidget(label)
+        yes_button_sad = QtWidgets.QPushButton("Ok :(")
+        yes_button_sad.clicked.connect(delete_dialog.reject)
+        layout.addWidget(yes_button_sad)
+        delete_dialog.exec()
+        return
+    label = QtWidgets.QLabel(f"Are you sure you want to delete the profile {profile_name}?")
+    yes_button = QtWidgets.QPushButton("Yes")
+    no_button = QtWidgets.QPushButton("No")
+    
+    layout.addWidget(label)
+    layout.addWidget(yes_button)
+    layout.addWidget(no_button)
+    
+    def on_yes_clicked():
+        global current_profile
+        profiles.delete_profile(f'profiles/{profile_name}.json')
+        current_profile = load_profile("default")
+        reload_buttons()
+        delete_dialog.accept()
+    
+    def on_no_clicked():
+        delete_dialog.reject()
+    
+    yes_button.clicked.connect(on_yes_clicked)
+    no_button.clicked.connect(on_no_clicked)
+    
+    delete_dialog.exec()
+
+def create_profile():
+    create_dialog = QtWidgets.QDialog()
+    create_dialog.setWindowTitle("Create Profile")
+    create_dialog.setWindowIcon(QtGui.QIcon("assets/icon.png"))
+    create_dialog.resize(200, 100)
+    layout = QtWidgets.QVBoxLayout()
+    create_dialog.setLayout(layout)
+    label = QtWidgets.QLabel("Enter the name of the profile")
+    input_field = QtWidgets.QLineEdit()
+    create_button = QtWidgets.QPushButton("Create")
+    cancel_button = QtWidgets.QPushButton("Cancel")
+    
+    layout.addWidget(label)
+    layout.addWidget(input_field)
+    layout.addWidget(create_button)
+    
+    def on_create_clicked():
+        global current_profile
+        profile_name = input_field.text()
+        profiles.create_profile(f'profiles/{profile_name}.json', profile_name)
+        current_profile = load_profile(profile_name)
+        reload_buttons()
+        create_dialog.accept()
+    
+    create_button.clicked.connect(on_create_clicked)
+    cancel_button.clicked.connect(create_dialog.reject)
+    
+    create_dialog.exec()
+
+def rename_profile(profile_name):
+    rename_dialog = QtWidgets.QDialog()
+    rename_dialog.setWindowTitle("Rename Profile")
+    rename_dialog.setWindowIcon(QtGui.QIcon("assets/icon.png"))
+    rename_dialog.resize(200, 100)
+    layout = QtWidgets.QVBoxLayout()
+    rename_dialog.setLayout(layout)
+    if profile_name == "default":
+        label = QtWidgets.QLabel("You cannot rename the default profile")
+        layout.addWidget(label)
+        yes_button_sad = QtWidgets.QPushButton("Ok :(")
+        yes_button_sad.clicked.connect(rename_dialog.reject)
+        layout.addWidget(yes_button_sad)
+        rename_dialog.exec()
+        return
+    label = QtWidgets.QLabel("Enter the new name of the profile")
+    input_field = QtWidgets.QLineEdit()
+    # add the current profile name to the input field
+    input_field.setText(profile_name)
+    rename_button = QtWidgets.QPushButton("Rename")
+    cancel_button = QtWidgets.QPushButton("Cancel")
+    
+    layout.addWidget(label)
+    layout.addWidget(input_field)
+    layout.addWidget(rename_button)
+    
+    def on_rename_clicked():
+        global current_profile
+        new_profile_name = input_field.text()
+        profiles.rename_profile(new_profile_name, current_profile, f'profiles/{profile_name}.json', f'profiles/{new_profile_name}.json')
+        current_profile = load_profile(new_profile_name)
+        reload_buttons()
+        rename_dialog.accept()
+    
+    rename_button.clicked.connect(on_rename_clicked)
+    cancel_button.clicked.connect(rename_dialog.reject)
+    
+    rename_dialog.exec()
 
 # Show the dialog and start the application event loop
 scroll_area = dialog.ideasList
@@ -252,8 +388,17 @@ description_scroll_area.setWidget(description_widget)
 exit_menu_bar = dialog.actionExit
 # Connect the "triggered" signal to the "close" slot, with PySide6 syntax
 exit_menu_bar.triggered.connect(app.quit)
+delete_profile_menu = dialog.actionDelete
+delete_profile_menu.triggered.connect(lambda: delete_dialog(current_profile.profile_name))
+
+create_profile_menu = dialog.actionAdd_new_profile
+create_profile_menu.triggered.connect(lambda: create_profile())
+
+rename_profile_menu = dialog.actionRename
+rename_profile_menu.triggered.connect(lambda: rename_profile(current_profile.profile_name))
 
 dialog.show()
+reload_buttons()
 
 # Set up keyboard event handlers
 for key in key_to_button.keys():
--- profiles.py
+++ profiles.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class Profile:
     def __init__(self, profile_name, layout):
@@ -28,6 +29,31 @@ def save_profile(profile, file_path):
     data = {
         'profileName': profile.profile_name,
         'layout': [item.to_dict() for item in profile.layout]
+    }
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def delete_profile(file_path):
+    os.remove(file_path)
+
+def rename_profile(new_name, profile, old_file_path, new_file_path):
+    os.rename(old_file_path, new_file_path)
+    data = {
+        'profileName': new_name,
+        'layout': [item.to_dict() for item in profile.layout]
+    }
+    with open(new_file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+
+def create_profile(file_path, profile_name):
+    data = {
+        'profileName': profile_name,
+        'layout': [{
+            "name": "",
+            "command": ""
+        }] * 18
     }
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
